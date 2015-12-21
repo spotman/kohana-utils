dc72e365a609e7f4d52ff9f2857ec191f271a438
Denis Terekhov <spotman@users.noreply.github.com>
2015-12-21 13:54:04 +0300

    Database_Query_Builder_Select::cached()

---
+++ classes/BetaKiller/Utils/Kohana/Database/Query/Biulder/Select.php
@@ -0,0 +1,35 @@
+<?php
+namespace \BetaKiller\Utils\Kohana;
+
+class Database_Query_Builder_Select extends Kohana_Database_Query_Builder_Select {
+    /**
+     * Кеширует результаты запроса
+     * Можно явно указать время жизни в секундах.
+     * Можно указать строковый ключ и тогда TTL будет взят из конфига, где ключом является имя первой переданной в метод from() таблицы + строковый ключ, явно определяющий запрос
+     * Можно не указывать ничего и тогда будет взят дефолтный TTL для таблицы
+     *
+     * @param int|string|null $lifetime Время жизни кеша в секундах или строковый ключ для запроса или NULL для дефолтного кеширования на 60 секунд
+     * @param bool $force
+     * @return $this
+     * @throws Kohana_Exception
+     */
+    public function cached($lifetime = NULL, $force = FALSE)
+    {
+        if (!Kohana::in_production())
+            return $this;
+
+        if (!$lifetime OR is_string($lifetime))
+        {
+            $key = Arr::get($this->_from, 0);
+
+            if (is_string($lifetime))
+            {
+                $key .= '.'.$lifetime;
+            }
+            $group = \Kohana::$config->load('clt');
+            $lifetime = $group ? $group->get($key, 60) : 60;
+        }
+
+        return parent::cached($lifetime, $force);
+    }
+}
