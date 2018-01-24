9cc257c6c04b02e7d5bb6ef517b20b384828e44c
Spotman <i.am@spotman.ru>
2018-01-24 15:40:58 +0800

    Fix UtilsException constructor (mimic Kohana_Exception behaviour)

--- classes/BetaKiller/Utils/UtilsException.php
+++ classes/BetaKiller/Utils/UtilsException.php
@@ -1,4 +1,28 @@
 <?php
 namespace BetaKiller\Utils;
 
-class UtilsException extends \Exception {}
+use Throwable;
+
+class UtilsException extends \Exception
+{
+    /**
+     * Construct the exception. Note: The message is NOT binary safe.
+     *
+     * @link  http://php.net/manual/en/exception.construct.php
+     *
+     * @param string     $message  [optional] The Exception message to throw.
+     * @param array|null $vars
+     * @param int        $code     [optional] The Exception code.
+     * @param Throwable  $previous [optional] The previous throwable used for the exception chaining.
+     *
+     * @since 5.1.0
+     */
+    public function __construct(string $message = '', array $vars = null, int $code = 0, \Throwable $previous = null)
+    {
+        if ($vars) {
+            $message = strtr($message, $vars);
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
